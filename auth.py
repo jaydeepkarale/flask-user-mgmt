bfe60624dce6b4b31d09bed17ece84658dcae962
Jaydeep Karale <97970643+jaydeepkarale@users.noreply.github.com>
2022-02-27 14:28:43 +0530

    First Commit

---
+++ auth.py
@@ -0,0 +1,53 @@
+from flask_login import login_user, logout_user
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
+
+auth = Blueprint('auth', __name__)
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    else:
+        username = request.form.get('username')
+        password = request.form.get('password')
+        remember = True if request.form.get('remember') else False
+
+        user = User.query.filter_by(username=username).first()
+
+        if not user or not check_password_hash(user.password, password):
+            flash('Please check your login details and try again.')
+            return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+    
+        login_user(user, remember=remember)
+        return redirect(url_for('main.profile'))
+
+@auth.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'GET':
+        return render_template('signup.html')
+    
+    else:
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        user = User.query.filter_by(username=username).first() 
+
+        if user: 
+            flash('Email address already exists')
+            return redirect(url_for('auth.signup'))
+
+        new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
+
+        db.session.add(new_user)
+        db.session.commit()
+
+    return redirect(url_for('auth.login'))
+
+@auth.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
